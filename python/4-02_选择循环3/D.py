c5b008791ad8ed8a0f842eaabb2f000f3426a012
fangxi1998 <fangxi1998@126.com>
2019-07-02 11:12:06 +0800

    update

--- python/4-02_选择循环3/D.py
+++ python/4-02_选择循环3/D.py
@@ -1,12 +1,12 @@
 sr1 = "abcdefghijklmnopqrstuvwxyz"
-sr=sr1+sr1
-in_str = input("")
+
+in_str = input()
 out_str = ""
 for j in in_str:
     if j == " ":
         out_str = out_str + " "
         continue
-    i = sr.find(j)
+    i = sr1.find(j)
     
-    out_str = out_str + sr[i+3]
+    out_str = out_str + sr1[(i+3)%26]
 print(out_str)
